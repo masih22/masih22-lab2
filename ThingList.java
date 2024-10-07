e84fb1a1fd56f477eb19fe11988da6ca8667c36d
Masih Faryadi <masih22@masihs-mbp.mynetworksettings.com>
2024-10-06 23:36:35 -0400

    Completed ThingList which includes a private node class, addThingNode method, moveAll, and printAll

---
+++ ThingList.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+public class ThingList {
+    //Node class
+    private class Node {
+        Thing data; 
+        Node next;
+
+        Node(Thing t){
+            this.data = t;
+            this.next = null;
+        }
+    }
+    
+    private Node head;
+    //method for adding a new node with a new thing
+    public void addThingNode(Thing t) {
+        Node createdNode = new Node(t);
+        createdNode.next = head; 
+        head = createdNode;
+    }
+    //method for printing all of things at certain row, col and color
+    public void printAll() {
+        Node currentNode = head;
+        while(currentNode != null){
+            System.out.println(currentNode.data.row + " " + currentNode.data.col + " " + currentNode.data.lab);
+            currentNode = currentNode.next;
+        }
+    }
+    //method for moving all things in list
+    public void moveAll(Random t) {
+        Node currentNode = head;
+        while(currentNode != null) {
+            currentNode.data.maybeTurn(t);
+            currentNode.data.step();
+            currentNode = currentNode.next;
+        }
+
+    }
+}
